867dd3bdebd1c86234a3ea92c168a979410e47ac
Bas van Beers <bas@scoretrace.com>
2018-05-09 19:40:16 +0200

    [PATCH] Cron update

--- src/messages/nl/cron.php
+++ src/messages/nl/cron.php
@@ -1,5 +1,23 @@
 <?php
 
-return [];
+return [
+    'Import' => 'Import',
+    'Paste crontab content' => 'Paste crontab content',
+    'Export' => 'Export',
+    'Path to PHP' => 'Path to PHP',
+    'Path to folder' => 'Path to folder',
+    'php file' => 'php bestand',
+    'ID' => "ID",
+    "Time" => "Time",
+    "Command" => "Command",
+    "Status" => "Status",
+    "Comment" => "Comment",
+    "Created" => "Created",
+    "Updated" => "Updated",
+    "Edit" => "Edit",
+    "Log" => "Log",
+    "Run" => "Run",
+    "With selected" => "With selected",
+];
 
 ?>
